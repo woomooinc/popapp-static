ab4d7b02ab1cef6450dfcd169c2bee46e65ecbdd
Hina Chen <hinablue@gmail.com>
2014-05-22 13:25:00 +0800

    Add mobile menu

--- source/js/main.js
+++ source/js/main.js
@@ -4,6 +4,88 @@
     if (carousel && carousel.style) {
         carousel.style.height = (window.innerHeight > 480 ? (window.innerHeight >= 900 ? 900 : window.innerHeight) : 480) + 'px';
     }
+
+    var slidemenu = document.getElementById('slidemenu');
+    slidemenu.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        var header = document.getElementById('header'),
+            container = document.getElementById('container'),
+            footer = document.getElementById('footer'),
+            menu = document.getElementById('mobile-menu');
+
+        if (menu.classList.contains('active')) {
+            header.style.width = '100%';
+            header.style.marginLeft = '0px';
+            container.style.width = '100%';
+            container.style.marginLeft = '0px';
+            footer.style.width = '100%';
+            footer.style.marginLeft = '0px';
+            menu.style.marginLeft = '100%';
+            menu.className = '';
+        } else {
+            header.style.width = window.innerWidth+'px';
+            header.style.marginLeft = 100 - window.innerWidth + 'px';
+            container.style.width = window.innerWidth+'px';
+            container.style.marginLeft = 100 - window.innerWidth + 'px';
+            footer.style.width = window.innerWidth+'px';
+            footer.style.marginLeft = 100 - window.innerWidth + 'px';
+            menu.style.marginLeft = '100px';
+            menu.className = 'active';
+        }
+    }, false);
+    var closeslidemenu = document.getElementById('close-mobile-menu');
+    closeslidemenu.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        var header = document.getElementById('header'),
+            container = document.getElementById('container'),
+            footer = document.getElementById('footer'),
+            menu = document.getElementById('mobile-menu');
+
+            header.style.width = '100%';
+            header.style.marginLeft = '0px';
+            container.style.width = '100%';
+            container.style.marginLeft = '0px';
+            footer.style.width = '100%';
+            footer.style.marginLeft = '0px';
+            menu.style.marginLeft = '100%';
+            menu.className = '';
+    }, false);
+
+    var countries = document.getElementById('countries');
+    countries.addEventListener('mouseenter', function(event) {
+        event.preventDefault();
+        var list = countries.children[0].childNodes, k = 0;
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].nodeName.toLowerCase() === 'li') {
+                if (list[i].classList.contains('active')) {
+                    list[i].style.top = '0px';
+                } else {
+                    list[i].style.top = (k * 2)+'em';
+                }
+                k++;
+            }
+        }
+    }, false);
+    countries.addEventListener('mouseleave', function(event) {
+        event.preventDefault();
+        var list = countries.children[0].childNodes;
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].nodeName.toLowerCase() === 'li') {
+                list[i].style.top = '0px';
+            }
+        }
+    }, false);
+    countries.addEventListener('mousecancel', function(event) {
+        event.preventDefault();
+        var list = countries.children[0].childNodes;
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].nodeName.toLowerCase() === 'li') {
+                list[i].style.top = '0px';
+            }
+        }
+    }, false);
     
     var header = document.getElementById('header');
     var mainHeaderTrigger = document.getElementById('popapp-landing');
@@ -11,6 +93,8 @@
     var isFloatHeader = false, isFloatStep = false;
 
     window.addEventListener('scroll', function(event) {
+        event.preventDefault();
+
         var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
         if (featureHeaderTrigger) {
             if (scrollTop > featureHeaderTrigger.offsetHeight) {
@@ -60,5 +144,5 @@
                 }
             }
         }
-    });
+    }, false);
 })();
